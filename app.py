0a0322325b0cda9d84f413a027637d0eb65c2202
lwang94 <lawrence.fy.wang@gmail.com>
2023-12-28 08:52:56 -0500

    new layout for free particle

--- app.py
+++ app.py
@@ -10,11 +10,60 @@ from layout_infinite_square_well import isw_layout
 app = DashProxy(transforms=[BlockingCallbackTransform(timeout=5)])
 
 app.layout = html.Div([
-    isw_layout()
+    # isw_layout(),
+    html.Div([
+            html.H1(
+                children="Free Particle",
+                style={"textAlign": "center"},
+                className="one row"
+            ),
+            html.Div(
+                children=[
+                    dcc.ConfirmDialog(
+                        id="invalid_custom_function",
+                        message="Invalid Custom Function"
+                    ),
+                    html.Div(
+                        children=[
+                            html.Pre("BOUND", style={"font-size": "18px"}),
+                            dcc.Input(
+                                id="bound_input",
+                                type="number",
+                                placeholder="bound",
+                                value=0.5,
+                                min=0.05,
+                                max=0.95,
+                                step=0.05,
+                                style={"width": "75%"}
+                            ),
+                            html.Pre("NUM PARTICLES", style={"font-size": "18px", "padding-top": "2%"}),
+                            dcc.Input(
+                                id="n_particles_input",
+                                type="number",
+                                placeholder="num particles",
+                                value=10,
+                                style={"width": "75%"}
+                            )
+                        ],
+                        style={"margin-left": "8%"},
+                        className="one half columns"
+                    ),
+                    html.Div(
+                        children=[
+                            dcc.Graph(
+                                id="probability_graph"
+                            )
+                        ],
+                        className="seven columns"
+                    )
+                ],
+                className="one row"
+            )
+        ])
 ])
 
-callbacks_graph(app)
-callbacks_widgets(app)
+# callbacks_graph(app)
+# callbacks_widgets(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
